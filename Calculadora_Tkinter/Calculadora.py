718fe30f10ec80ad01280afeb1ab81478bfe5173
Santy <santiago01prado@gmail.com>
2023-08-07 19:14:31 -0300

    Cambios en Calculadora, Agregado de Potencia y Raiz

--- Calculadora_Tkinter/Calculadora.py
+++ Calculadora_Tkinter/Calculadora.py
@@ -1,49 +1,9 @@
 from tkinter import * 
 import math
+from funciones import *
 
-i = 0
 ventana = Tk()
 ventana.title('Calculadora')
-
-#Funciones
-def click_boton(valor):
-    global i 
-    entrada_texto.insert(i, valor)
-    i += 1
-
-def borrar():
-    entrada_texto.delete(0,END)
-    i = 0
-
-def resultado():
-    ecuacion = entrada_texto.get()
-    # Reemplazar '^' por '**'
-    ecuacion = ecuacion.replace('^', '**') 
-    
-    try:
-        igual = eval(ecuacion)
-        entrada_texto.delete(0, END)
-        entrada_texto.insert(0, igual)
-        i = 0
-    except:
-        # Verificar si hay una raíz cuadrada
-        try:
-            if '√' in ecuacion: 
-                # Reemplazar para la función sqrt 
-                ecuacion = ecuacion.replace('√', 'math.sqrt(')  
-                igual = eval(ecuacion+')')
-                
-                entrada_texto.delete(0, END)
-                entrada_texto.insert(0, igual)
-                i = 0
-            else:
-                entrada_texto.delete(0, END)
-                entrada_texto.insert(0, "Error")
-                i = 0
-        except:
-            entrada_texto.delete(0, END)
-            entrada_texto.insert(0, "Error")
-            i = 0
 
 #Entrada de Texto
 
@@ -52,71 +12,73 @@ entrada_texto.grid(row=0, column = 0, columnspan = 4, padx = 5, pady = 5 )
 
 #Botones Numeros 0 al 9
 
-boton0 = Button(ventana, text= '0', width= 13 , height= 2, command= lambda: click_boton(0))
+boton0 = Button(ventana, text= '0', width= 13 , height= 2, command= lambda: click_boton(0, entrada_texto))
 boton0.grid(row = '5', column = '0',columnspan=2, padx = 5, pady = 5)
 
-boton1 = Button(ventana, text= '1', width= 5, height= 2, command= lambda: click_boton(1))
+boton1 = Button(ventana, text= '1', width= 5, height= 2, command= lambda: click_boton(1, entrada_texto))
 boton1.grid(row = '4', column = '0', padx = 5, pady = 5)
 
-boton2 = Button(ventana, text= '2', width= 5, height= 2, command= lambda: click_boton(2))
+boton2 = Button(ventana, text= '2', width= 5, height= 2, command= lambda: click_boton(2, entrada_texto))
 boton2.grid(row = '4', column = '1', padx = 5, pady = 5)
 
-boton3 = Button(ventana, text= '3', width= 5, height= 2, command= lambda: click_boton(3))
+boton3 = Button(ventana, text= '3', width= 5, height= 2, command= lambda: click_boton(3, entrada_texto))
 boton3.grid(row = '4', column = '2', padx = 5, pady = 5)
 
-boton4 = Button(ventana, text= '4', width= 5, height= 2, command= lambda: click_boton(4))
+boton4 = Button(ventana, text= '4', width= 5, height= 2, command= lambda: click_boton(4, entrada_texto))
 boton4.grid(row = '3', column = '0', padx = 5, pady = 5)
 
-boton5 = Button(ventana, text= '5', width= 5, height= 2, command= lambda: click_boton(5))
+boton5 = Button(ventana, text= '5', width= 5, height= 2, command= lambda: click_boton(5, entrada_texto))
 boton5.grid(row = '3', column = '1', padx = 5, pady = 5)
 
-boton6 = Button(ventana, text= '6', width= 5, height= 2, command= lambda: click_boton(6))
+boton6 = Button(ventana, text= '6', width= 5, height= 2, command= lambda: click_boton(6, entrada_texto))
 boton6.grid(row = '3', column = '2', padx = 5, pady = 5)
 
-boton7 = Button(ventana, text= '7', width= 5, height= 2, command= lambda: click_boton(7))
+boton7 = Button(ventana, text= '7', width= 5, height= 2, command= lambda: click_boton(7, entrada_texto))
 boton7.grid(row = '2', column = '0', padx = 5, pady = 5)
 
-boton8 = Button(ventana, text= '8', width= 5, height= 2, command= lambda: click_boton(8))
+boton8 = Button(ventana, text= '8', width= 5, height= 2, command= lambda: click_boton(8, entrada_texto))
 boton8.grid(row = '2', column = '1', padx = 5, pady = 5)
 
-boton9 = Button(ventana, text= '9', width= 5, height= 2, command= lambda: click_boton(9))
+boton9 = Button(ventana, text= '9', width= 5, height= 2, command= lambda: click_boton(9, entrada_texto))
 boton9.grid(row = '2', column = '2', padx = 5, pady = 5)
 
 #Botones Operaciones Matematicas
 
-boton_suma = Button(ventana, text = '+', width= 5, height=2, command= lambda: click_boton('+'))
+boton_suma = Button(ventana, text = '+', width= 5, height=2, command= lambda: click_boton('+', entrada_texto))
 boton_suma.grid(row = '3', column = '3', padx = 5, pady = 5)
 
-boton_resta = Button(ventana, text = '-', width= 5, height=2, command= lambda: click_boton('-'))
+boton_resta = Button(ventana, text = '-', width= 5, height=2, command= lambda: click_boton('-', entrada_texto))
 boton_resta.grid(row = '4', column = '3', padx = 5, pady = 5)
 
-boton_multiplicacion = Button(ventana, text = '*', width= 5, height=2, command= lambda: click_boton('*'))
+boton_multiplicacion = Button(ventana, text = '*', width= 5, height=2, command= lambda: click_boton('*', entrada_texto))
 boton_multiplicacion.grid(row = '2', column = '3', padx = 5, pady = 5)
 
-boton_division = Button(ventana, text = '/', width= 5, height=2, command= lambda: click_boton('/'))
+boton_division = Button(ventana, text = '/', width= 5, height=2, command= lambda: click_boton('/', entrada_texto))
 boton_division.grid(row = '1', column = '3', padx = 5, pady = 5)
 
-boton_potencia = Button(ventana, text='^',width= 5, height=2, command= lambda: click_boton('^'))
+boton_potencia = Button(ventana, text='^',width= 5, height=2, command= lambda: click_boton('^', entrada_texto))
 boton_potencia.grid(row='1', column='4', padx=5, pady=5)
 
-boton_raiz = Button(ventana, text='√', width=5, height=2, command=lambda: click_boton('√'))
+boton_raiz = Button(ventana, text='√', width=5, height=2, command=lambda: click_boton('√', entrada_texto))
 boton_raiz.grid(row='2', column='4', padx=5, pady=5)
 
 #Botones Extra
 
-boton_igual = Button(ventana, text = '=', width= 5, height=2, command= lambda: resultado())
+boton_igual = Button(ventana, text = '=', width= 5, height=2, command= lambda: resultado(entrada_texto.get(),entrada_texto, ventana))
 boton_igual.grid(row = '5', column = '3', padx = 5, pady = 5)
 
-boton_borrar = Button(ventana, text = 'AC', width= 5, height=2, command= lambda: borrar())
+boton_borrar = Button(ventana, text = 'AC', width= 5, height=2, command= lambda: borrar(entrada_texto))
 boton_borrar.grid(row = '1', column = '0', padx = 5, pady = 5)
 
-boton_parentesis1 = Button(ventana, text = '(', width= 5, height=2, command= lambda: click_boton('('))
+boton_parentesis1 = Button(ventana, text = '(', width= 5, height=2, command= lambda: click_boton('(', entrada_texto))
 boton_parentesis1.grid(row = '1', column = '1', padx = 5, pady = 5)
 
-boton_parentesis2 = Button(ventana, text = ')', width= 5, height=2, command= lambda: click_boton(')'))
+boton_parentesis2 = Button(ventana, text = ')', width= 5, height=2, command= lambda: click_boton(')', entrada_texto))
 boton_parentesis2.grid(row = '1', column = '2', padx = 5, pady = 5)
 
-boton_punto = Button(ventana, text = '.', width= 5, height=2, command= lambda: click_boton('.'))
+boton_punto = Button(ventana, text = '.', width= 5, height=2, command= lambda: click_boton('.', entrada_texto))
 boton_punto.grid(row = '5', column = '2', padx = 5, pady = 5)
+
+
 
 ventana.mainloop()
---
+++ Calculadora_Tkinter/funciones.py
@@ -0,0 +1,104 @@
+import sqlite3
+import math
+from tkinter import *
+
+# Conexión a la base de datos (creará un archivo si no existe)
+base_datos = sqlite3.connect('Calculadora_Tkinter/historial.bd')
+
+# Crear tabla de tareas si no existe
+base_datos.execute('''
+    CREATE TABLE IF NOT EXISTS historial (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cuenta TEXT NOT NULL,
+        resultado TEXT NOT NULL
+    )
+''')
+
+
+base_datos.commit()
+base_datos.close()
+
+i = 0
+
+def click_boton(valor, entrada_texto):
+    global i
+    entrada_texto.insert(i, valor)
+    i += 1
+    return i
+
+def borrar(entrada_texto):
+    entrada_texto.delete(0,END)
+    i = 0
+    return i
+
+def resultado(ecuacion, entrada_texto, ventana):
+    ecuacion = entrada_texto.get()
+    # Reemplazar '^' por '**'
+    ecuacion = ecuacion.replace('^', '**') 
+    
+    try:
+        igual = eval(ecuacion)
+        entrada_texto.delete(0, END)
+        entrada_texto.insert(0, igual)
+        i = 0
+    except:
+        # Verificar si hay una raíz cuadrada
+        try:
+            if '√' in ecuacion: 
+                # Reemplazar para la función sqrt 
+                ecuacion = ecuacion.replace('√', 'math.sqrt(')  
+                igual = eval(ecuacion+')')
+                
+                entrada_texto.delete(0, END)
+                entrada_texto.insert(0, igual)
+                i = 0
+            else:
+                entrada_texto.delete(0, END)
+                entrada_texto.insert(0, "Error")
+                i = 0
+        except:
+            entrada_texto.delete(0, END)
+            entrada_texto.insert(0, "Error")
+            i = 0
+
+    conn = sqlite3.connect('Calculadora_Tkinter/historial.bd')
+    conn.execute('INSERT INTO historial (cuenta, resultado) VALUES (?, ?)', (ecuacion, igual))
+    conn.commit()
+    conn.close()
+    mostrar_historial(ventana)
+    return(igual, i)
+
+
+# Función para mostrar el historial
+def mostrar_historial(ventana):
+    boton_eliminar_historial = Button(ventana, text='Eliminar Historial', command= lambda: eliminar_historial(ventana))
+    boton_eliminar_historial.grid(row='5', column='5')
+
+    conn = sqlite3.connect('Calculadora_Tkinter/historial.bd')
+    cursor = conn.execute('SELECT * FROM historial')
+    historial = cursor.fetchall()
+    conn.close()
+    
+    texto_mostrar = Label(ventana, text='Historial:')
+    texto_mostrar.grid(row=0, column=5)
+    # Usar un índice para la posición de la fila en la ventana
+    fila = 1
+    
+    for registro in historial:
+        id, cuenta, resultado = registro  # Desempaquetar la tupla del registro
+        
+        etiqueta_cuenta = Label(ventana, text=f'{cuenta}')
+        etiqueta_resultado = Label(ventana, text=f'Resultado: {resultado}')
+        
+        etiqueta_cuenta.grid(row=fila, column=5, sticky=W)
+        etiqueta_resultado.grid(row=fila, column=6, sticky=W)
+        
+        fila += 1
+
+def eliminar_historial(ventana):
+    conn = sqlite3.connect('Calculadora_Tkinter/historial.bd')
+    conn.execute('DELETE FROM historial')
+    conn.commit()
+    conn.close()
+    mostrar_historial(ventana)
+    
