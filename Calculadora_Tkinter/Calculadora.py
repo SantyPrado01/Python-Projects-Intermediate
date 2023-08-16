243dfecf763680e92b821acc118729d57e5e83e2
SantyPrado01 <santiago01prado@gmail.com>
2023-08-15 22:10:08 -0300

    Calculadora Completada

--- Calculadora_Tkinter/Calculadora.py
+++ Calculadora_Tkinter/Calculadora.py
@@ -1,86 +1,93 @@
 from tkinter import * 
+from tkinter.font import Font
 import math
 from funciones import *
+
+
 
 ventana = Tk()
 ventana.title('Calculadora')
 
+ventana.configure(bg='black')
+ventana.iconbitmap('Calculadora_Tkinter\icono.ico')
+ventana.resizable(width=False, height=False)
+fuente_botones =('Arial',14)
 #Entrada de Texto
 
-entrada_texto = Entry(ventana, font = ('Calibri 16'), justify=RIGHT)
-entrada_texto.grid(row=0, column = 0, columnspan = 5)
+entrada_texto = Entry(ventana, font =('Arial', 22), justify=RIGHT, bg='black', fg='white')
+entrada_texto.grid(row=0, column = 0, columnspan=4)
 
 #Botones Numeros 0 al 9
 
-boton0 = Button(ventana, text= '0', width= 12 , height= 2, command= lambda: click_boton(0, entrada_texto))
-boton0.grid(row = '5', column = '0',columnspan=2)
+boton0 = Button(ventana,font=fuente_botones, text= '0', width= 14 , height= 2, command= lambda: click_boton(0, entrada_texto), bg='#363636', fg='white')
+boton0.grid(row = '7', column = '0',columnspan=2)
 
-boton1 = Button(ventana, text= '1', width= 5, height= 2, command= lambda: click_boton(1, entrada_texto))
-boton1.grid(row = '4', column = '0')
+boton1 = Button(ventana, text= '1',font=fuente_botones, width= 6, height= 2, command= lambda: click_boton(1, entrada_texto), bg='#363636', fg='white')
+boton1.grid(row = '6', column = '0')
 
-boton2 = Button(ventana, text= '2', width= 5, height= 2, command= lambda: click_boton(2, entrada_texto))
-boton2.grid(row = '4', column = '1')
+boton2 = Button(ventana, text= '2',font=fuente_botones, width= 6, height= 2, command= lambda: click_boton(2, entrada_texto), bg='#363636', fg='white')
+boton2.grid(row = '6', column = '1')
 
-boton3 = Button(ventana, text= '3', width= 5, height= 2, command= lambda: click_boton(3, entrada_texto))
-boton3.grid(row = '4', column = '2')
+boton3 = Button(ventana, text= '3',font=fuente_botones, width= 6, height= 2, command= lambda: click_boton(3, entrada_texto), bg='#363636', fg='white')
+boton3.grid(row = '6', column = '2')
 
-boton4 = Button(ventana, text= '4', width= 5, height= 2, command= lambda: click_boton(4, entrada_texto))
-boton4.grid(row = '3', column = '0')
+boton4 = Button(ventana, text= '4',font=fuente_botones, width= 6, height= 2, command= lambda: click_boton(4, entrada_texto), bg='#363636', fg='white')
+boton4.grid(row = '5', column = '0')
 
-boton5 = Button(ventana, text= '5', width= 5, height= 2, command= lambda: click_boton(5, entrada_texto))
-boton5.grid(row = '3', column = '1')
+boton5 = Button(ventana, text= '5',font=fuente_botones, width= 6, height= 2, command= lambda: click_boton(5, entrada_texto), bg='#363636', fg='white')
+boton5.grid(row = '5', column = '1')
 
-boton6 = Button(ventana, text= '6', width= 5, height= 2, command= lambda: click_boton(6, entrada_texto))
-boton6.grid(row = '3', column = '2')
+boton6 = Button(ventana, text= '6',font=fuente_botones, width= 6, height= 2, command= lambda: click_boton(6, entrada_texto), bg='#363636', fg='white')
+boton6.grid(row = '5', column = '2')
 
-boton7 = Button(ventana, text= '7', width= 5, height= 2, command= lambda: click_boton(7, entrada_texto))
-boton7.grid(row = '2', column = '0')
+boton7 = Button(ventana, text= '7',font=fuente_botones, width= 6, height= 2, command= lambda: click_boton(7, entrada_texto), bg='#363636', fg='white')
+boton7.grid(row = '4', column = '0')
 
-boton8 = Button(ventana, text= '8', width= 5, height= 2, command= lambda: click_boton(8, entrada_texto))
-boton8.grid(row = '2', column = '1')
+boton8 = Button(ventana, text= '8',font=fuente_botones, width= 6, height= 2, command= lambda: click_boton(8, entrada_texto), bg='#363636', fg='white')
+boton8.grid(row = '4', column = '1')
 
-boton9 = Button(ventana, text= '9', width= 5, height= 2, command= lambda: click_boton(9, entrada_texto))
-boton9.grid(row = '2', column = '2')
+boton9 = Button(ventana, text= '9',font=fuente_botones, width= 6, height= 2, command= lambda: click_boton(9, entrada_texto), bg='#363636', fg='white')
+boton9.grid(row = '4', column = '2')
 
 #Botones Operaciones Matematicas
 
-boton_suma = Button(ventana, text = '+',  width=5, height=5,command= lambda: click_boton('+', entrada_texto))
-boton_suma.grid(row = '4', column = '3', rowspan=2)
+boton_suma = Button(ventana, text = '+',font=fuente_botones,  width=6, height=2,command= lambda: click_boton('+', entrada_texto), bg='#252525', fg='white')
+boton_suma.grid(row = '5', column = '3')
 
-boton_resta = Button(ventana, text = '-', width= 5, height=2, command= lambda: click_boton('-', entrada_texto))
-boton_resta.grid(row = '3', column = '4')
+boton_resta = Button(ventana, text = '-',font=fuente_botones, width= 6, height=2, command= lambda: click_boton('-', entrada_texto), bg='#252525', fg='white')
+boton_resta.grid(row = '4', column = '3')
 
-boton_multiplicacion = Button(ventana, text = 'X', width= 5, height=2, command= lambda: click_boton('*', entrada_texto))
+boton_multiplicacion = Button(ventana, text = 'X',font=fuente_botones, width= 6, height=2, command= lambda: click_boton('*', entrada_texto), bg='#252525', fg='white')
 boton_multiplicacion.grid(row = '3', column = '3')
 
-boton_division = Button(ventana, text = '/', width= 5, height=2, command= lambda: click_boton('/', entrada_texto))
-boton_division.grid(row = '1', column = '4')
+boton_division = Button(ventana, text = '/',font=fuente_botones, width= 6, height=2, command= lambda: click_boton('/', entrada_texto), bg='#252525', fg='white')
+boton_division.grid(row = '3', column = '2')
 
-boton_potencia = Button(ventana, text='^',width= 5, height=2, command= lambda: click_boton('^', entrada_texto))
-boton_potencia.grid(row='2', column='3')
+boton_potencia = Button(ventana, text='^',font=fuente_botones,width= 6, height=2, command= lambda: click_boton('^', entrada_texto), bg='#252525', fg='white')
+boton_potencia.grid(row='3', column='1')
 
-boton_raiz = Button(ventana, text='√', width=5, height=2, command=lambda: click_boton('√', entrada_texto))
-boton_raiz.grid(row='2', column='4')
+boton_raiz = Button(ventana, text='√',font=fuente_botones, width=6, height=2, command=lambda: click_boton('√', entrada_texto), bg='#252525', fg='white')
+boton_raiz.grid(row='3', column='0')
 
 #Botones Extra
 
-boton_igual = Button(ventana, text = '=', width= 5, height=5, command= lambda: resultado(entrada_texto.get(),entrada_texto, ventana))
-boton_igual.grid(row = '4', column = '4',rowspan=2)
+boton_igual = Button(ventana, text = '=',font=fuente_botones, width= 6, height=5, command= lambda: resultado(entrada_texto.get(),entrada_texto, ventana), bg='#2C94BE', fg='white')
+boton_igual.grid(row = '6', column = '3',rowspan=2)
 
-boton_borrar = Button(ventana, text = 'AC', width= 5, height=2, command= lambda: borrar(entrada_texto))
-boton_borrar.grid(row = '1', column = '0')
+boton_borrar = Button(ventana, text = 'AC',font=fuente_botones, width= 6, height=2, command= lambda: borrar(entrada_texto), bg='#252525', fg='white')
+boton_borrar.grid(row = '2', column = '0')
 
-boton_borrar_uno = Button(ventana, text='←', width=5, height=2,command= lambda: borrar_uno(entrada_texto))
-boton_borrar_uno.grid(row='1', column='3')
+boton_borrar_uno = Button(ventana, text='←',font=fuente_botones, width=6, height=2,command= lambda: borrar_uno(entrada_texto), bg='#252525', fg='white')
+boton_borrar_uno.grid(row='2', column='3')
 
-boton_parentesis1 = Button(ventana, text = '(', width= 5, height=2, command= lambda: click_boton('(', entrada_texto))
-boton_parentesis1.grid(row = '1', column = '1')
+boton_parentesis1 = Button(ventana, text = '(',font=fuente_botones, width= 6, height=2, command= lambda: click_boton('(', entrada_texto), bg='#252525', fg='white')
+boton_parentesis1.grid(row = '2', column = '1')
 
-boton_parentesis2 = Button(ventana, text = ')', width= 5, height=2, command= lambda: click_boton(')', entrada_texto))
-boton_parentesis2.grid(row = '1', column = '2')
+boton_parentesis2 = Button(ventana, text = ')',font=fuente_botones, width= 6, height=2, command= lambda: click_boton(')', entrada_texto), bg='#252525', fg='white')
+boton_parentesis2.grid(row = '2', column = '2')
 
-boton_punto = Button(ventana, text = '.', width= 5, height=2, command= lambda: click_boton('.', entrada_texto))
-boton_punto.grid(row = '5', column = '2')
+boton_punto = Button(ventana, text = '.',font=fuente_botones, width= 6, height=2, command= lambda: click_boton('.', entrada_texto), bg='#252525', fg='white')
+boton_punto.grid(row = '7', column = '2')
 
 
 
--- Calculadora_Tkinter/funciones.py
+++ Calculadora_Tkinter/funciones.py
@@ -3,7 +3,7 @@ import math
 from tkinter import *
 
 # Conexión a la base de datos (creará un archivo si no existe)
-base_datos = sqlite3.connect('Calculadora_Tkinter/historial.bd')
+base_datos = sqlite3.connect('historial.bd')
 
 # Crear tabla de tareas si no existe
 base_datos.execute('''
@@ -13,7 +13,6 @@ base_datos.execute('''
         resultado TEXT NOT NULL
     )
 ''')
-
 
 base_datos.commit()
 base_datos.close()
@@ -49,6 +48,7 @@ def resultado(ecuacion, entrada_texto, ventana):
         entrada_texto.delete(0, END)
         entrada_texto.insert(0, igual)
         i = 0
+        borrar(entrada_texto)
     except:
         # Verificar si hay una raíz cuadrada
         try:
@@ -60,14 +60,17 @@ def resultado(ecuacion, entrada_texto, ventana):
                 entrada_texto.delete(0, END)
                 entrada_texto.insert(0, igual)
                 i = 0
+                borrar(entrada_texto)
             else:
                 entrada_texto.delete(0, END)
                 entrada_texto.insert(0, "Error")
                 i = 0
+                borrar(entrada_texto)
         except:
             entrada_texto.delete(0, END)
             entrada_texto.insert(0, "Error")
             i = 0
+            borrar(entrada_texto)
 
     conn = sqlite3.connect('Calculadora_Tkinter/historial.bd')
     conn.execute('INSERT INTO historial (cuenta, resultado) VALUES (?, ?)', (ecuacion, igual))
@@ -79,15 +82,15 @@ def resultado(ecuacion, entrada_texto, ventana):
 
 # Función para mostrar el historial
 def mostrar_historial(ventana):
-    boton_eliminar_historial = Button(ventana, text='Eliminar Historial', command= lambda: eliminar_historial(ventana))
-    boton_eliminar_historial.grid(row='5', column='5')
+    boton_eliminar_historial = Button(ventana, text='Eliminar Historial', command= lambda: eliminar_historial(ventana), font=('Arial',14))
+    boton_eliminar_historial.grid(row='7', column='5')
 
     conn = sqlite3.connect('Calculadora_Tkinter/historial.bd')
     cursor = conn.execute('SELECT * FROM historial')
     historial = cursor.fetchall()
     conn.close()
     
-    texto_mostrar = Label(ventana, text='Historial:')
+    texto_mostrar = Label(ventana, text='Historial:', bg='black', fg='white',font=('Arial',22))
     texto_mostrar.grid(row=0, column=5)
     # Usar un índice para la posición de la fila en la ventana
     fila = 1
@@ -95,8 +98,8 @@ def mostrar_historial(ventana):
     for registro in historial:
         id, cuenta, resultado = registro  # Desempaquetar la tupla del registro
         
-        etiqueta_cuenta = Label(ventana, text=f'{cuenta}')
-        etiqueta_resultado = Label(ventana, text=f'Resultado: {resultado}')
+        etiqueta_cuenta = Label(ventana, text=f'{cuenta}', bg='black', fg='white',font=('Arial',14))
+        etiqueta_resultado = Label(ventana, text=f'Resultado: {resultado}', bg='black', fg='white',font=('Arial',14))
         
         etiqueta_cuenta.grid(row=fila, column=5, sticky=W)
         etiqueta_resultado.grid(row=fila, column=6, sticky=W)
